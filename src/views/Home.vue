33db3cd458b5dbf8cbae06ceb3fae3ec57c4092e
Chantelsky <laver.chantelsky@gmail.com>
2021-04-10 20:07:47 +1000

    feat: setting up

---
+++ src/views/Home.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="home">
+    <h1>Weather App</h1>
+    <div class="enter-city">
+      <input v-model="data.city" type="text" placeholder="Enter a city" />
+    </div>
+    <div class="weather">
+      <h1>6&deg;</h1>
+      <h2>Cloudy</h2>
+      <h3>Lots of clouds</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import { reactive } from 'vue';
+
+export default {
+  setup() {
+    const data = reactive({
+      city: '',
+      weather: null,
+    });
+    return {
+      data,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+.enter-city {
+  input {
+    font-size: 40px;
+  }
+}
+.weather {
+  margin-top: 10px;
+  h1 {
+    font-size: 80px;
+  }
+  h1,
+  h2,
+  h3 {
+    margin: 0;
+  }
+}
+</style>
